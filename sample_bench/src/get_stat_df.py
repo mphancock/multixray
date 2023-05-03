e257e205667c62215752948f787cdf15cb747766
mphancock <matthewhancock97@gmail.com>
2023-05-02 22:52:56 -0700

    update get_stat_df to return pdb stats
    get_stat_df now returns additional columns corresponding to the file and entry that can be used to identify the corresponding pdb file. It also has an additional offset parameter that can be used to only compute stats on frames with a corresponding pdb file.

--- sample_bench/src/get_stat_df.py
+++ sample_bench/src/get_stat_df.py
@@ -5,12 +5,62 @@ import multiprocessing
 
 
 """
+get_stat_from_log_df takes a log_df and returns a statistic (eg, mean) of a given field post equilibration.
+
+params
+log_df: pandas dataframe, log file as a dataframe.
+field: str, name of the field to compute the statistic on.
+stat: str, name of the statistic to compute.
+N: int, the number of values to return for a stat. Only applicable to min and max.
+
+returns
+field_stat_df: pandas dataframe, dataframe containing the stat value(s) plus entry information. The dataframe will only be greater than a single row for min and max with N>1.
+"""
+def get_stat_from_log_df(
+        log_df,
+        field,
+        stat,
+        N
+):
+    field_stat_df = pd.DataFrame(columns=["field", "stat", "value", "log", "id"])
+
+    # 3 error cases to eturn nan entries: if the field is not in the log_df, if the log_df is empty, or if the stat is not invalid.
+    if field not in log_df.columns:
+        field_stat_df.loc[0] = [field, stat, np.nan, np.nan, np.nan]
+    elif len(log_df) == 0:
+        field_stat_df.loc[0] = [field, stat, np.nan, np.nan, np.nan]
+    else:
+        if stat in ["min", "max"]:
+            if stat == "min":
+                n_stat_df = log_df.nsmallest(N, field)
+            else:
+                n_stat_df = log_df.nlargest(N, field)
+
+            for i in range(len(n_stat_df)):
+                field_stat_df.loc[i] = [field, stat, n_stat_df[field].iloc[i], n_stat_df["log"].iloc[i], n_stat_df["id"].iloc[i]]
+        elif stat == "mean":
+            field_stat_df.loc[0] = [field, stat, log_df[field].mean(), np.nan, np.nan]
+        elif stat == "std":
+            field_stat_df.loc[0] = [field, stat, log_df[field].std(), np.nan, np.nan]
+        elif stat == "var":
+            field_stat_df.loc[0] = [field, stat, log_df[field].var(), np.nan, np.nan]
+        else:
+            field_stat_df.loc[0] = [field, stat, np.nan, np.nan, np.nan]
+
+    return field_stat_df
+
+
+"""
 get_stat_from_log_files takes a list of log_files and returns a statistic (eg, mean) of a given field post equilibration.
 
+params
 log_files: list, list of log files.
 equil: int, number of lines to skip in the log file.
 field: str, name of the field to compute the statistic on.
 stat: str, name of the statistic to compute.
+
+returns
+field_stat_df: pandas dataframe, dataframe containing the stat value(s) plus entry information. The dataframe will only be greater than a single row for min and max with N>1.
 """
 def pool_get_stat_from_log_files(
         params_dict
@@ -19,105 +69,81 @@ def pool_get_stat_from_log_files(
     equil = params_dict["equil"]
     field = params_dict["field"]
     stat = params_dict["stat"]
+    N = params_dict["N"]
+    offset = params_dict["offset"]
 
     # print(log_files, equil, field, stat)
-    log_files, field, stat, log_stat, stat_id = get_stat_from_log_files(
+    log_files, field_stat_df = get_stat_from_log_files(
         log_files=log_files,
         equil=equil,
         field=field,
-        stat=stat
+        stat=stat,
+        N=N,
+        offset=offset
     )
 
-    return log_files, field, stat, log_stat, stat_id
+    return log_files, field_stat_df
 
 
 def get_stat_from_log_files(
         log_files,
-        equil,
         field,
-        stat
+        stat,
+        equil,
+        N,
+        offset
 ):
     # Merge the log files into a single dataframe.
     log_dfs = list()
     for log_file in log_files:
         try:
-            # print("trying to read log file: {}".format(log_file))
+            # Need to add an entry that corresponds to the original log file and the original index.
             log_df = pd.read_csv(log_file)
+            log_df["log"] = [log_file]*len(log_df)
+            log_df["id"] = list(log_df.index)
         except pd.errors.EmptyDataError:
-            # print("catching empty data error")
             continue
-        log_dfs.append(log_df[equil:])
+        log_dfs.append(log_df[equil::offset])
 
     # Return np.nan if there are no log files or the length of the merged log file is 0.
     if len(log_dfs) == 0:
-        log_stat, stat_id = np.nan, np.nan
+        field_stat_df = pd.DataFrame(columns=["field", "stat", "value", "log", "id"])
+        field_stat_df.loc[0] = [field, stat, np.nan, np.nan, np.nan]
     else:
         merge_log_df = pd.concat(log_dfs)
-        if len(merge_log_df) == 0:
-            log_stat, stat_id = np.nan, np.nan
-        else:
-            log_stat, stat_id = get_stat_from_log_df(
-                    log_df=merge_log_df,
-                    field=field,
-                    stat=stat
-                )
+        field_stat_df = get_stat_from_log_df(
+                log_df=merge_log_df,
+                field=field,
+                stat=stat,
+                N=N
+            )
 
-    return log_files, field, stat, log_stat, stat_id
-
-
-"""
-get_stat_from_log_df takes a log_df and returns a statistic (eg, mean) of a given field post equilibration.
-
-log_df: pandas dataframe, log file as a dataframe.
-field: str, name of the field to compute the statistic on.
-stat: str, name of the statistic to compute.
-"""
-def get_stat_from_log_df(
-        log_df,
-        field,
-        stat,
-):
-    if field == "tot":
-        log_df["tot"] = 30000*log_df["xray"] + log_df["ff"]
-
-    stat_id = np.nan
-    if field not in log_df.columns:
-        log_stat = np.nan
-    else:
-        if stat == "min":
-            log_stat = log_df[field].min()
-            stat_id = log_df[field].idxmin()
-        elif stat == "max":
-            log_stat = log_df[field].max()
-            stat_id = log_df[field].idxmax()
-        elif stat == "mean":
-            log_stat = log_df[field].mean()
-        elif stat == "std":
-            log_stat = log_df[field].std()
-        elif stat == "var":
-            log_stat = log_df[field].var()
-        else:
-            log_stat = np.nan
-    # else:
-    #     raise RuntimeError("Invalid stat selection: {}".format(stat))
-
-    return log_stat, stat_id
+    return log_files, field_stat_df
 
 
 """
 get_stat_df function takes in a set of output directories (presumably the set of output directories for a job) and returns a dataframe containing the stat for each field for each log file for all stat, field pair. Having this dataframe is important for sample volume benchmarks.
 
+params
 log_file_groups: the set of groups where each group contains one or more log files (the rows of the stat_df). Each stat, field pair is computed for each group of log files.
 fields: the fields (eg, rmsd) from each log file to extract a statistic for.
 stats: the statistic (eg, mean) to compute for each field (field[i]_statistic[i] is a column).
+N: the number of entries to return for each field, stat pair. This only applies to min and max.
+offset: only look return stats for log entries with offset frequency. This is useful when we only want entries that have a corresponding pdb file.
 equil: the number of frames to discard before computing a statistic.
-include_id: whether to include an additional column for each requested min/max which records the relevant file.
+test: additional param to get the stat_df without multiprocessing.
+
+returns
+stat_df: pandas dataframe, dataframe with each log file group as entires and the requested stats as columns. The type and number of returned columns depends on the stat type as well as other parameters.
 """
 def get_stat_df(
         log_file_groups,
         fields,
         stats,
-        equil
+        N,
+        offset,
+        equil,
+        test=False
 ):
     if len(fields) != len(stats):
         raise RuntimeError("The number of fields ({}) does not match the number of stats ({})".format(len(fields), len(stats)))
@@ -127,10 +153,15 @@ def get_stat_df(
     # Construct the columns for the stat_df.
     columns = list()
     for i in range(n_fields):
-        column = "{}_{}".format(fields[i], stats[i])
-        columns.append(column)
         if stats[i] in ["min", "max"]:
-            columns.append("{}_id".format(column))
+            for j in range(N):
+                column = "{}_{}_{}".format(fields[i], stats[i], j)
+                columns.append(column)
+                columns.append("{}_log".format(column))
+                columns.append("{}_id".format(column))
+        else:
+            column = "{}_{}".format(fields[i], stats[i])
+            columns.append(column)
 
     indices = [str(group) for group in log_file_groups]
     log_stat_df = pd.DataFrame(index=indices, columns=columns)
@@ -145,6 +176,8 @@ def get_stat_df(
             pool_param["equil"] = equil
             pool_param["field"] = fields[i]
             pool_param["stat"] = stats[i]
+            pool_param["N"] = N
+            pool_param["offset"] = offset
             pool_params.append(pool_param)
 
     # print("CPUs: {}".format(multiprocessing.cpu_count()))
@@ -152,22 +185,43 @@ def get_stat_df(
         multiprocessing.cpu_count()
     )
 
-    pool_results = pool_obj.imap(
-        pool_get_stat_from_log_files,
-        pool_params
-    )
+    if test:
+        pool_results = list()
+        for pool_param in pool_params:
+            pool_results.append(pool_get_stat_from_log_files(pool_param))
+    else:
+        pool_results = pool_obj.imap(
+            pool_get_stat_from_log_files,
+            pool_params
+        )
 
     # Iterate through the pool results and fill in the stat_df.
     # print(log_stat_df.head())
-    for pool_result in pool_results:
-        log_files, field, stat, log_stat, stat_id = pool_result
-        column = "{}_{}".format(field, stat)
-        entry = str(log_files)
-        log_stat_df.loc[entry, column] = log_stat
+    for log_files, field_stat_df in pool_results:
+        # print(field_stat_df.head())
+        field = field_stat_df.iloc[0]["field"]
+        stat = field_stat_df.iloc[0]["stat"]
 
-        id_column = "{}_id".format(column)
-        if id_column in log_stat_df.columns:
-            log_stat_df.loc[entry, id_column] = stat_id
+        entry = str(log_files)
+
+        for i in range(len(field_stat_df)):
+            if stat in ["min", "max"]:
+                col = "{}_{}_{}".format(field, stat, i)
+            else:
+                col = "{}_{}".format(field, stat, i)
+
+            val = field_stat_df.iloc[i]["value"]
+
+            log_stat_df.loc[entry, col] = val
+
+            log_file_col = "{}_log".format(col)
+            id_col = "{}_id".format(col)
+            if log_file_col in log_stat_df.columns:
+                log_file = field_stat_df.iloc[i]["log"]
+                entry_id = field_stat_df.iloc[i]["id"]
+
+                log_stat_df.loc[entry, log_file_col] = log_file
+                log_stat_df.loc[entry, id_col] = entry_id
 
     pool_obj.close()
     return log_stat_df
--- sample_bench/unit_tests/get_stat_df_test.py
+++ sample_bench/unit_tests/get_stat_df_test.py
@@ -15,29 +15,29 @@ class TestGetStatDF(unittest.TestCase):
         self.log_file_groups = [[Path(data_dir, "log_0.csv"), Path(data_dir, "log_1.csv")], [Path(data_dir, "log_2.csv")]]
         self.fields = ['xray', 'r_free', 'ff']
         self.stats = ['mean', 'std', 'min']
-        self.equil = 100
+        self.N = 5
+        self.offset = 5
+        self.equil = 10
 
     def test_get_stat_df_returns_dataframe(self):
         # Ensure that the function returns a pandas dataframe
-        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.equil)
+        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.N, self.offset, self.equil)
         self.assertIsInstance(stat_df, pd.DataFrame)
-
-    def test_get_stat_df_returns_expected_shape(self):
-        # Ensure that the shape of the returned dataframe matches the expected shape
-        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.equil)
-        expected_shape = (2, 4)
-        self.assertEqual(stat_df.shape, expected_shape)
 
     def test_get_stat_df_returns_expected_columns(self):
         # Ensure that the columns of the returned dataframe match the expected columns
-        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.equil)
+        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.N, self.offset, self.equil)
         columns = stat_df.columns.tolist()
-        expected_columns = ['xray_mean', 'r_free_std', 'ff_min', 'ff_min_id']
-        self.assertEqual(columns, expected_columns)
+        exp_cols = ['xray_mean', 'r_free_std']
+        for i in range(self.N):
+            exp_cols.append("ff_min_{}".format(i))
+            exp_cols.append("ff_min_{}_log".format(i))
+            exp_cols.append("ff_min_{}_id".format(i))
+        self.assertEqual(columns, exp_cols)
 
     def test_get_stat_df_returns_expected_index(self):
         # Ensure that the index of the returned dataframe matches the expected index
-        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.equil)
+        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.N, self.offset, self.equil)
         index = stat_df.index.tolist()
         expected_index = [str(log_file_group) for log_file_group in self.log_file_groups]
         self.assertEqual(index, expected_index)
@@ -45,34 +45,46 @@ class TestGetStatDF(unittest.TestCase):
     def test_get_stat_df_raises_error_with_invalid_number_of_fields(self):
         # Ensure that an error is raised if the number of fields is not equal to the number of stats
         with self.assertRaises(RuntimeError):
-            get_stat_df(self.log_file_groups, ['xray', 'r_free'], self.stats, self.equil)
+            get_stat_df(self.log_file_groups, ['xray', 'r_free'], self.stats, self.N, self.offset, self.equil)
 
     def test_get_stat_df_with_invalid_stats(self):
         # Ensure that if an invalid stat is passed that the returned value is np.nan
-        stat_df = get_stat_df(self.log_file_groups, self.fields, ['mean', 'min', 'invalid_stat'], self.equil)
+        stat_df = get_stat_df(self.log_file_groups, self.fields, ['mean', 'min', 'invalid_stat'], self.N, self.offset, self.equil)
         for i in range(stat_df.shape[0]):
             self.assertTrue(pd.isna(stat_df["ff_invalid_stat"].iloc[i]))
 
     def test_get_stat_df_with_invalid_field(self):
         # Ensure that if an invalid field is passed that the returned value is np.nan
-        stat_df = get_stat_df(self.log_file_groups, ['xray', 'r_free', 'invalid_field'], self.stats, self.equil)
+        stat_df = get_stat_df(self.log_file_groups, ['xray', 'invalid_field', 'r_free'], self.stats, self.N, self.offset, self.equil)
         for i in range(stat_df.shape[0]):
-            self.assertTrue(pd.isna(stat_df["invalid_field_min"].iloc[i]))
+            self.assertTrue(pd.isna(stat_df["invalid_field_std"].iloc[i]))
 
     def test_get_stat_df_when_equil_greater_than_length(self):
         # Ensure that if the dataframe is empty or equil is greater than size of the dataframe that returned values are np.nan
-        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, 1000)
+        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.N, self.offset, 1000)
         for i in range(stat_df.shape[0]):
             for j in range(stat_df.shape[1]):
                 self.assertTrue(pd.isna(stat_df.iloc[i, j]))
 
     def test_get_stat_df_when_empty(self):
         # Ensure that if the passed file is an empty file that returned values are np.nan
-        stat_df = get_stat_df([[Path(Path.home(), "xray/sample_bench/unit_tests/data/log_empty.csv")]], self.fields, self.stats, self.equil)
+        stat_df = get_stat_df([[Path(Path.home(), "xray/sample_bench/unit_tests/data/log_empty.csv")]], self.fields, self.stats, self.N, self.offset, self.equil)
         for i in range(stat_df.shape[0]):
             for j in range(stat_df.shape[1]):
                 self.assertTrue(pd.isna(stat_df.iloc[i, j]))
 
+    def test_get_stat_df(self):
+        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.N, 1, 0)
+        self.assertAlmostEqual(stat_df["xray_mean"].iloc[0], 5.625173381290343)
+        self.assertAlmostEqual(stat_df["r_free_std"].iloc[0], 0.02690603303822287)
+        self.assertAlmostEqual(stat_df["ff_min_0"].iloc[0], 160.939455, delta=1e-5)
+        self.assertAlmostEqual(stat_df["ff_min_1"].iloc[0], 161.779053, delta=1e-5)
+        self.assertEqual(stat_df["ff_min_0_log"].iloc[0], Path(Path.home(), "xray/sample_bench/unit_tests/data/log_1.csv"))
+        self.assertEqual(stat_df["ff_min_0_id"].iloc[0], 6)
+
+    def test_get_stat_df_equil_and_offset(self):
+        stat_df = get_stat_df(self.log_file_groups, self.fields, self.stats, self.N, self.offset, self.equil)
+        self.assertAlmostEqual(stat_df["xray_mean"].iloc[1], 5.695093119734579)
 
 if __name__ == '__main__':
     unittest.main()
