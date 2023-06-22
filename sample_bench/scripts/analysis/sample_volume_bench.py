7ad8c74ceb9346bb942d2c402fb04648c07f696e
mphancock <matthewhancock97@gmail.com>
2023-06-22 08:28:07 -0700

    Updated sample bench src/scripts to use new get_stat_df

---
+++ sample_bench/scripts/analysis/sample_volume_bench.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import multiprocessing
+import pickle
+import argparse
+import time
+import sys
+
+sys.path.append(str(Path(Path.home(), "xray/sample_bench/src")))
+import get_stat_df
+import sample_bench
+sys.path.append(str(Path(Path.home(), "xray/src")))
+import align_imp
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--job_dir")
+    parser.add_argument("--sample_bench_dir")
+    parser.add_argument("--ref_pdb_file")
+    parser.add_argument("--field")
+    parser.add_argument("--bonus_fields")
+    parser.add_argument("--max_n", type=int)
+    args = parser.parse_args()
+    print(args.job_dir)
+    print(args.sample_bench_dir)
+    print(args.ref_pdb_file)
+    print(args.field)
+    print(args.bonus_fields)
+    print(args.max_n)
+
+    job_dir = Path(args.job_dir)
+    out_dirs = list(job_dir.glob("output*"))
+    out_dirs = out_dirs
+
+    sample_bench_dir = Path(args.sample_bench_dir)
+    sample_bench_dir.mkdir(parents=True, exist_ok=True)
+
+    # First, construct the lookup table that contains for each log_file entry, the minimum value for that MD log for each of the requested fields.
+    # Need to run an offset of 10 to get pdb structures only.
+    max_frames = list()
+    log_files = list()
+    for out_dir in out_dirs:
+        log_files.append(Path(out_dir, "log.csv"))
+
+        pdb_dir = Path(out_dir, "pdbs")
+        n_pdbs = len(list(pdb_dir.glob("*.pdb")))
+        max_frame = (n_pdbs-1)*10
+        max_frames.append(max_frame)
+
+    bonus_fields = args.bonus_fields.split(",")
+    score_field = args.field
+    score_stat_df = get_stat_df.get_stat_df(
+        log_file_groups=[[log_file] for log_file in log_files],
+        main_field=score_field,
+        main_stat="min",
+        bonus_fields=bonus_fields,
+        N=1,
+        offset=10,
+        equil=100,
+        max_frames=max_frames
+    )
+    score_stat_df.to_csv(Path(sample_bench_dir, "stat_df_{}.csv".format(score_field)))
+    # score_stat_df = pd.read_csv(Path(out_file.parents[0], "score_stat_df.csv"), index_col=0)
+
+    if args.max_n > len(log_files):
+        max_n = len(log_files)
+    else:
+        max_n = args.max_n
+
+    sample_bench_bonus_fields = bonus_fields.copy()
+    if "pdb" in sample_bench_bonus_fields:
+        sample_bench_bonus_fields.remove("pdb")
+
+    # Next, create m random groups of n log files to compute the mean and standard deviation of the minimum field values of the log files in the group. m and n are both set to the number of total log files.
+    sample_volume_df = sample_bench.get_sample_volume_df(
+        log_files=log_files,
+        log_min_df=score_stat_df,
+        score_field=score_field,
+        extra_fields=sample_bench_bonus_fields,
+        max_n=max_n
+    )
+    sample_volume_df.to_csv(Path(sample_bench_dir, "sample_bench_{}.csv".format(score_field)))
--- sample_bench/scripts/analysis/sample_volume_bench.sh
+++ sample_bench/scripts/analysis/sample_volume_bench.sh
@@ -6,21 +6,27 @@
 #$ -l mem_free=5G
 #$ -l scratch=5G
 #$ -N vol_bench
-#$ -pe smp 50
+#$ -pe smp 25
 #$ -l hostname='qb3-id*'
 
 
-module load CBI conda-stage
-conda activate imp_218_cctbx
+# module load CBI conda-stage
+# conda activate imp_218_cctbx
 
-JOB_NAME="14_32_h20"
-JOB_ID="3608088"
-MAX_N="1000"
+JOB_NAME="46_synth_sa_3"
+REF_PDB_FILE="/wynton/home/sali/mhancock/xray/data/pdbs/7mhf/7mhf_refine.pdb"
+JOB_ID="9417275_xray"
+MAX_N="1"
+FIELD="xray_0"
+BONUS_FIELDS="pdb,rmsd"
+# FIELD="rmsd"
+# BONUS_FIELDS="pdb"
 
-JOB_DIR="/wynton/group/sali/mhancock/xray/sample_bench/out/7mhk/$JOB_NAME/$JOB_ID"
-OUT_FILE="/wynton/home/sali/mhancock/xray/sample_bench/data/7mhk/$JOB_NAME/sample_bench.csv"
+JOB_DIR="/wynton/group/sali/mhancock/xray/sample_bench/out/7mhf/$JOB_NAME/$JOB_ID"
+SAMPLE_BENCH_DIR="/wynton/home/sali/mhancock/xray/sample_bench/data/7mhf/$JOB_NAME"
+# OUT_FILE="/wynton/home/sali/mhancock/xray/sample_bench/data/7mhf/$JOB_NAME/sample_bench_xray.csv"
 
-python run_sample_volume_bench.py --job_dir "$JOB_DIR" --out_file "$OUT_FILE" --max_n "$MAX_N"
+python sample_volume_bench.py --job_dir "$JOB_DIR" --sample_bench_dir "$SAMPLE_BENCH_DIR" --ref_pdb_file "$REF_PDB_FILE" --field "$FIELD" --bonus_fields  "$BONUS_FIELDS" --max_n "$MAX_N"
 
 [[ -n "$TMPDIR" ]] && qstat -j "$JOB_ID"
 trap 'conda deactivate' EXIT
