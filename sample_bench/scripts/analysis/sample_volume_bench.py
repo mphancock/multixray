fba384402551fea41a0a706321dcba5a309a92a2
mphancock <matthewhancock97@gmail.com>
2023-06-27 14:20:19 -0700

    Sampling of the log_min_df to generate groups
    In order to generate groups of size N, we now directly sample the log_min_df to return subsets of size N.

--- sample_bench/scripts/analysis/sample_volume_bench.py
+++ sample_bench/scripts/analysis/sample_volume_bench.py
@@ -9,9 +9,115 @@ import sys
 
 sys.path.append(str(Path(Path.home(), "xray/sample_bench/src")))
 import get_stat_df
-import sample_bench
-sys.path.append(str(Path(Path.home(), "xray/src")))
-import align_imp
+
+
+"""
+Return all_group_mins which is a dataframe with 1000 rows corresponding to the 1000 groups of size n and columns corresponding to the score_field plus extra_fields. The values are the best score plus corresponding extra field values for each group.
+
+The group contains the indices of the log_files that are to be grouped together.
+"""
+def get_all_group_mins(
+        param_dict
+):
+    n = param_dict["n"]
+    log_files = param_dict["log_files"]
+    log_min_df = param_dict["log_min_df"]
+    score_field = param_dict["score_field"]
+    extra_fields = param_dict["extra_fields"]
+
+    fields = [score_field]
+    fields.extend(extra_fields)
+
+    # Dataframe for the best score plus extra fields for each group (1000 total).
+    all_group_mins_df = pd.DataFrame(index=list(range(1000)), columns=fields)
+
+    # Get only the size n group subset.
+    for i in range(2000):
+        group_df = log_min_df.sample(n=n)
+        min_entry = group_df.loc[group_df["{}_min_0".format(score_field)] == group_df["{}_min_0".format(score_field)].min()]
+        all_group_mins_df.loc[i, score_field] = min_entry["{}_min_0".format(score_field)].values[0]
+
+        for field in extra_fields:
+            all_group_mins_df.loc[i, field] = min_entry["{}_min_0_{}".format(score_field, field)].values[0]
+
+    print(n)
+    return n, all_group_mins_df
+
+
+"""
+This function performs the core sample benchmark. 1000 random groups of size n are created for n between 1 and the maximum number of logs. For each group, the best score (as evaluated by field score_field) is computed (1000 total). For all groups of size n, the mean best score as well as the variance of the best scores. The distribution of other parameters (extra_fields) for the best scoring entries from the groups can be computed as well. To avoid redudnant calculations of the best scoring structure for each log_file, a lookup table is passed as well (log_min_df).
+
+**********
+Parameters:
+
+    log_files: list of Path objects to the log files.
+
+    log_min_df: pandas dataframe containing the minimum value for each log file. It has the format of a stat_df (columns: [field_min_0, extra_fields] and rows: [[log_file]]).
+
+**********
+Returns:
+
+    log_stats_df: pandas dataframe with the mean and standard deviation of the best score for each group of size N. The columns are [N, (score_field)_mean, (score_field)_std, (extra_fields)_mean, (extra_fields)_std].
+
+"""
+def get_sample_volume_df(
+        log_files,
+        log_min_df,
+        score_field,
+        extra_fields,
+        max_n
+):
+    log_stats_cols = list()
+    log_stats_cols.append("n")
+
+    fields = [score_field]
+    fields.extend(extra_fields)
+    for field in fields:
+        log_stats_cols.append("{}_mean".format(field))
+        log_stats_cols.append("{}_std".format(field))
+
+    log_stats_df = pd.DataFrame(index=list(range(1,max_n+1)), columns=log_stats_cols)
+
+    pool_obj = multiprocessing.Pool(
+        multiprocessing.cpu_count()
+    )
+
+    pool_params = list()
+    for n in range(1,max_n+1):
+        params_dict = dict()
+        params_dict["n"] = n
+        params_dict["log_files"] = log_files
+        params_dict["log_min_df"] = log_min_df
+        params_dict["score_field"] = score_field
+        params_dict["extra_fields"] = extra_fields
+        pool_params.append(params_dict)
+
+    print("CPUs: {}".format(multiprocessing.cpu_count()))
+    t0 = time.time()
+
+    # pool_results = list()
+    # for pool_param in pool_params:
+    #     pool_results.append(get_all_group_mins(pool_param))
+
+    pool_results = pool_obj.imap(
+        get_all_group_mins,
+        pool_params
+    )
+
+    for result in pool_results:
+        n, all_group_mins_df = result
+        for field in fields:
+            field_mean = np.mean(all_group_mins_df[field])
+            field_std = np.std(all_group_mins_df[field])
+
+            log_stats_df.loc[n, "{}_mean".format(field)] = field_mean
+            log_stats_df.loc[n, "{}_std".format(field)] = field_std
+
+    print(time.time()-t0)
+
+    pool_obj.close()
+
+    return log_stats_df
 
 
 if __name__ == "__main__":
@@ -21,14 +127,12 @@ if __name__ == "__main__":
     parser.add_argument("--ref_pdb_file")
     parser.add_argument("--field")
     parser.add_argument("--bonus_fields")
-    parser.add_argument("--max_n", type=int)
     args = parser.parse_args()
     print(args.job_dir)
     print(args.sample_bench_dir)
     print(args.ref_pdb_file)
     print(args.field)
     print(args.bonus_fields)
-    print(args.max_n)
 
     job_dir = Path(args.job_dir)
     out_dirs = list(job_dir.glob("output*"))
@@ -57,21 +161,16 @@ if __name__ == "__main__":
     score_stat_df.to_csv(Path(sample_bench_dir, "stat_df_{}.csv".format(score_field)))
     # score_stat_df = pd.read_csv(Path(sample_bench_dir, "stat_df_{}.csv".format(score_field)), index_col=0)
 
-    if args.max_n > len(log_files):
-        max_n = len(log_files)
-    else:
-        max_n = args.max_n
-
     sample_bench_bonus_fields = bonus_fields.copy()
     if "pdb" in sample_bench_bonus_fields:
         sample_bench_bonus_fields.remove("pdb")
 
     # Next, create m random groups of n log files to compute the mean and standard deviation of the minimum field values of the log files in the group. m and n are both set to the number of total log files.
-    sample_volume_df = sample_bench.get_sample_volume_df(
+    sample_volume_df = get_sample_volume_df(
         log_files=log_files,
         log_min_df=score_stat_df,
         score_field=score_field,
         extra_fields=sample_bench_bonus_fields,
-        max_n=max_n
+        max_n=len(score_stat_df)
     )
     sample_volume_df.to_csv(Path(sample_bench_dir, "sample_bench_{}.csv".format(score_field)))
--- sample_bench/scripts/analysis/sample_volume_bench.sh
+++ sample_bench/scripts/analysis/sample_volume_bench.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 
-JOB_NAME="50_synth_sa_5"
+JOB_NAME="51_synth_sa_5"
 REF_PDB_FILE="/wynton/home/sali/mhancock/xray/data/pdbs/7mhf/7mhf_refine.pdb"
-JOB_ID="9479402"
-MAX_N="1000"
+JOB_ID="9485465"
 JOB_DIR="/wynton/group/sali/mhancock/xray/sample_bench/out/7mhf/$JOB_NAME/$JOB_ID"
 SAMPLE_BENCH_DIR="/wynton/home/sali/mhancock/xray/sample_bench/data/7mhf/$JOB_NAME"
 
 
 FIELD="xray_0"
 BONUS_FIELDS="pdb,rmsd"
-# python sample_volume_bench.py --job_dir "$JOB_DIR" --sample_bench_dir "$SAMPLE_BENCH_DIR" --ref_pdb_file "$REF_PDB_FILE" --field "$FIELD" --bonus_fields  "$BONUS_FIELDS" --max_n "$MAX_N"
+python sample_volume_bench.py --job_dir "$JOB_DIR" --sample_bench_dir "$SAMPLE_BENCH_DIR" --ref_pdb_file "$REF_PDB_FILE" --field "$FIELD" --bonus_fields  "$BONUS_FIELDS"
 
 FIELD="rmsd"
 BONUS_FIELDS="pdb"
-python sample_volume_bench.py --job_dir "$JOB_DIR" --sample_bench_dir "$SAMPLE_BENCH_DIR" --ref_pdb_file "$REF_PDB_FILE" --field "$FIELD" --bonus_fields  "$BONUS_FIELDS" --max_n "$MAX_N"
+python sample_volume_bench.py --job_dir "$JOB_DIR" --sample_bench_dir "$SAMPLE_BENCH_DIR" --ref_pdb_file "$REF_PDB_FILE" --field "$FIELD" --bonus_fields  "$BONUS_FIELDS"
 
 [[ -n "$TMPDIR" ]] && qstat -j "$JOB_ID"
 trap 'conda deactivate' EXIT
