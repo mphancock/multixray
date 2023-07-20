855876deea7f3e6c8c3c7eb3fb02c95ad991d922
mphancock <matthewhancock97@gmail.com>
2023-07-20 16:28:04 -0700

    new optimizer state plus restraint for updating weights.

--- src/update_weights_optimizer_state.py
+++ src/update_weights_optimizer_state.py
@@ -20,13 +20,18 @@ class UpdateWeightsOptimizerState(IMP.OptimizerState):
     def do_update(self, call):
         # First, update w from w_grads.
         w_grads = self.r_xtal.get_w_grads()
+        print(w_grads)
 
-        w_grads_scale = [w_grads[i]*1000 for i in range(len(w_grads))]
+        # Normalize
+        for i in range(len(w_grads)):
+            w_grads[i] = w_grads[i]-w_grads[-1]
+
+        # w_grads_scale = [w_grads[i]*1000 for i in range(len(w_grads))]
         ws = self.w.get_weights()
         for i in range(len(ws)):
-            ws[i] = ws[i] + w_grads_scale[i]
+            ws[i] = ws[i] + w_grads[i]
         self.w.set_weights(ws)
-        print(w_grads_scale)
+        print(w_grads)
         print(self.w.get_weights())
 
         # Then, update the atomic structures.
@@ -38,6 +43,49 @@ class UpdateWeightsOptimizerState(IMP.OptimizerState):
                 at.set_occupancy(self.w.get_weight(i))
 
 
+class OptimizeWeightsOptimizerState(IMP.OptimizerState):
+    def __init__(
+        self,
+        m,
+        wr,
+        r_xray,
+        n_state,
+        step_tracker
+    ):
+        IMP.OptimizerState.__init__(self, m, "OptimizeWeightsOptimizerState%1%")
+        self.m = m
+        self.wr = wr
+        self.r_xray = r_xray
+        self.n_state = n_state
+        self.step_tracker = step_tracker
+
+        self.w = self.wr.get_w()
+        self.on = True
+
+    def get_on(self):
+        return self.on
+
+    def set_on(
+        self,
+        on
+    ):
+        self.on = on
+
+    def do_update(self, call):
+        if self.on and self.step_tracker.get_step() > 0:
+        # if self.on and self.r_xray.get_r_work() < .2:
+            self.wr.reset()
+            sf = IMP.core.RestraintsScoringFunction([self.wr])
+            # sf.evaluate(True)
+
+            cg = IMP.core.ConjugateGradients(self.m)
+            cg.set_scoring_function(sf)
+            cg.optimize(10)
+
+            best_occs = self.wr.get_best_occs()
+            self.w.set_weights(best_occs)
+        # else:
+        #     self.w.set_weights([1/self.n_state]*self.n_state)
 
 
 
---
+++ src/weight_restraint.py
@@ -0,0 +1,139 @@
+import numpy as np
+import IMP
+import IMP.core
+import IMP.atom
+import mmtbx
+
+import xray_struct
+
+class WeightRestraint(IMP.Restraint):
+    def __init__(
+            self,
+            m,
+            hs,
+            w,
+            f_obs,
+            flags,
+            scale
+    ):
+        IMP.Restraint.__init__(self, m, "weight")
+        self.m = m
+        self.hs = hs
+        self.w = w
+        self.n_state = len(self.hs)
+        self.f_obs = f_obs
+        self.flags = flags
+        self.scale = scale
+
+        self.best_f = np.infty
+        self.best_occs = [self.w.get_weight(i) for i in range(self.n_state)]
+
+    def get_scale(
+            self
+    ):
+        return self.scale
+
+    def get_best_occs(
+            self
+    ):
+        return self.best_occs
+
+    def get_w(
+            self
+    ):
+        return self.w
+
+    def set_scale(
+            self,
+            scale
+    ):
+        self.scale = scale
+
+    def reset(
+            self
+    ):
+        self.best_f = np.infty
+        self.best_occs = [self.w.get_weight(i) for i in range(self.n_state)]
+
+    def do_add_score_and_derivatives(
+            self,
+            sa
+    ):
+        # Update all occupancies based on weight of first pid.
+        # n_atoms = len(self.pids)/self.n_state
+
+        for i in range(self.n_state):
+            occ = self.w.get_weight(i)
+            pids = IMP.atom.Selection(self.hs[i]).get_selected_particle_indexes()
+            for pid in pids:
+                IMP.atom.Atom(self.m, pid).set_occupancy(occ)
+
+            print(occ)
+
+        xray_structure = xray_struct.get_xray_structure(
+            m=self.m,
+            crystal_symmetry=self.f_obs.crystal_symmetry()
+        )
+
+        xray_structure.scatterers().flags_set_grads(
+            state=False
+        )
+        xray_structure.scatterers().flags_set_grad_site(
+            iselection=xray_structure.all_selection().iselection()
+        )
+        xray_structure.scatterers().flags_set_grad_occupancy(
+            iselection=xray_structure.all_selection().iselection()
+        )
+
+        f_model_manager = mmtbx.f_model.manager(
+            xray_structure=xray_structure,
+            f_obs=self.f_obs,
+            r_free_flags=self.flags,
+            target_name="ml"
+        )
+        f_model_manager.update_all_scales()
+        fmodels = mmtbx.fmodels(fmodel_xray=f_model_manager)
+
+        fmodels.create_target_functors()
+        fmodels.prepare_target_functors_for_minimization()
+
+        fmodels.update_xray_structure(update_f_calc = True)
+        fmodels_target_and_gradients = fmodels.target_and_gradients(
+            weights=None,
+            compute_gradients=True,
+            occupancy=True
+        )
+
+        f = fmodels_target_and_gradients.target()
+        if f < self.best_f:
+            self.best_f = f
+            self.best_occs = [self.w.get_weight(i) for i in range(self.n_state)]
+
+        g =  fmodels_target_and_gradients.gradients()
+        occ_gs = list()
+
+        n_atoms = len(IMP.atom.Selection(self.hs[0]).get_selected_particle_indexes())
+        for i in range(self.n_state):
+            occ_gs.append(g[i*n_atoms:(i+1)*n_atoms])
+
+        avg_gs = list()
+        # Get the average.
+        for i in range(self.n_state):
+            # avg_g = sum(occ_gs[i])/n_atoms
+            avg_g = sum(occ_gs[i])
+            avg_gs.append(avg_g)
+
+        # Normalize.
+        state_gs = list()
+        for i in range(self.n_state):
+            state_gs.append(avg_gs[i]-avg_gs[-1])
+
+        print(state_gs)
+        sa.add_score(f)
+        if sa.get_derivative_accumulator():
+            for i in range(self.n_state):
+                self.w.add_to_weight_derivative(i, self.get_scale()*state_gs[i], sa.get_derivative_accumulator())
+
+    def do_get_inputs(self):
+        w_pid = self.w.get_particle_index()
+        return [self.get_model().get_particle(w_pid)]
