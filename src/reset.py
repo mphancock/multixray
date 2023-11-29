270a0629ce9a9dedf6aa88783ac2455cb8886e29
mphancock <matthewhancock97@gmail.com>
2023-11-29 11:49:00 -0800

    manually set gradients and velocities plus better logging
    The system has a tendency to quickly jump back to high energy states following the reset. The new implementation will set all velocities and gradients to 0. It will also set the temperature of both the md engine and thermostat. Not a total fix.

--- src/reset.py
+++ src/reset.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+from pathlib import Path
 
 import IMP
 import IMP.core
@@ -8,6 +8,9 @@ import IMP.atom
 import trackers
 
 
+"""
+Doesn't work perfectly. The system has a tendency to quickly jump back to high energy states following the rest.
+"""
 class ResetTracker(trackers.Tracker):
     def __init__(
             self,
@@ -29,6 +32,7 @@ class ResetTracker(trackers.Tracker):
         self.w = w
         self.r_charmm = r_charmm
         self.md = None
+        self.sv = None
         self.sa_sched = sa_sched
         self.h_0 = h_0
         self.step = 0
@@ -41,6 +45,12 @@ class ResetTracker(trackers.Tracker):
             md
     ):
         self.md = md
+
+    def set_sv(
+            self,
+            sv
+    ):
+        self.sv = sv
 
     def reset_positions(self):
         pids_0 = IMP.atom.Selection(self.h_0).get_selected_particle_indexes()
@@ -55,6 +65,13 @@ class ResetTracker(trackers.Tracker):
                 xyz_0 = IMP.core.XYZ(self.h_0.get_model(), pids_0[i])
 
                 xyz.set_coordinates(xyz_0.get_coordinates())
+
+                # Set the derivatives and velocities to 0.
+                da = IMP.DerivativeAccumulator()
+                d = IMP.core.XYZR(self.get_model(), pids[0])
+                d.add_to_derivatives(-d.get_derivatives(), da)
+
+                IMP.atom.LinearVelocity(self.m, pids[i]).set_velocity([0,0,0])
 
 
     def determine_current_T(self):
@@ -73,8 +90,13 @@ class ResetTracker(trackers.Tracker):
 
     def reset_velocities(self):
         T = self.determine_current_T()
+        T=300
+
+        # Need to take care of the scaling optimizer and the md engine.
         print("RESETTING TO {}K".format(T))
-        self.md.assign_velocities(T)
+        self.md.set_temperature(T)
+        self.sv.set_temperature(T)
+        # self.md.assign_velocities(T)
 
 
     def do_evaluate(self):
@@ -86,13 +108,24 @@ class ResetTracker(trackers.Tracker):
     def evaluate(
             self
     ):
-        ff_score = self.r_charmm.evaluate(False)
+        # ff_score = self.r_charmm.evaluate(False)
+        ff_score = self.r_charmm.get_last_score()
 
         # We need this first condition because the tracker will be evaluated before the MD has been set.
         if self.step > 0 and self.step % self.get_period() == 0 and self.get_on() and ff_score > 1e5 and self.get_md():
             reset = self.do_evaluate()
+            IMP.atom.write_multimodel_pdb(self.hs, str(Path(Path.home(), "xray/tmp/tmp.pdb")))
+            # ff_score = self.r_charmm.evaluate(False)
         else:
             reset = 0
+
+        if self.step > 0:
+            pids = IMP.atom.Selection(self.hs[0]).get_selected_particle_indexes()
+            print("T: {}".format(self.md.get_temperature()))
+            print("KE: {}".format(self.md.get_kinetic_energy()))
+            print("ff: {}".format(ff_score))
+            print("gradients: {}".format(IMP.core.XYZR(self.m, pids[0]).get_derivatives()))
+            print("velocity: {}".format(IMP.atom.LinearVelocity(self.m, pids[0]).get_velocity()))
 
         self.step = self.step+1
 
