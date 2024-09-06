d73a7d3ae0459d0a1d31f36fc5c4d5d2469ba251
mphancock <matthewhancock97@gmail.com>
2024-09-06 13:27:27 -0500

    added ability to only update xray score/derivs every N frames

--- src/xray_restraint.py
+++ src/xray_restraint.py
@@ -21,6 +21,7 @@ class XtalRestraint(IMP.Restraint):
             update_scale,
             update_k1,
             u_aniso_file,
+            update_freq,
             ref_com=None
     ):
         IMP.Restraint.__init__(self, msmc_m.get_m(), "XrayRestraint%1%")
@@ -29,6 +30,8 @@ class XtalRestraint(IMP.Restraint):
         self.n_state = len(self.hs)
         self.pids = msmc_m.get_all_pids()
         self.cond = cond
+        self.update_freq = update_freq
+        self.n_evals = 0
 
         self.f_obs = f_obs
         self.free_flags = free_flags
@@ -87,6 +90,9 @@ class XtalRestraint(IMP.Restraint):
     def set_occs(self, occs):
         self.occs = occs
 
+    def set_update_freq(self, update_freq):
+        self.update_freq = update_freq
+
     def get_weight(self):
         return self.w_xray
 
@@ -111,6 +117,9 @@ class XtalRestraint(IMP.Restraint):
     def get_r_all(self):
         return self.r_all
 
+    def get_update_freq(self):
+        return self.update_freq
+
     def do_add_score_and_derivatives(self, sa):
         # Get the reference COM.
         if self.ref_com:
@@ -121,57 +130,64 @@ class XtalRestraint(IMP.Restraint):
         # print("DELTA: ", self.delta)
 
         # Get the derivatives.
-        results_dict = cctbx_score.get_score(
-            hs=self.msmc_m.get_hs(),
-            occs=self.msmc_m.get_w_mat()[:,self.cond],
-            pids=self.pids,
-            f_obs=self.f_obs_filt,
-            r_free_flags=self.flags_filt,
-            target=self.target,
-            update_scale=self.update_scale,
-            update_k1=self.update_k1,
-            u_aniso_file=self.u_aniso_file,
-            delta=self.delta
-        )
 
-        score = results_dict["score"]
-        grads_site = results_dict["grads_site"]
-        grads_occ = results_dict["grads_occ"]
-        r_work = results_dict["r_work"]
-        r_free = results_dict["r_free"]
-        r_all = results_dict["r_all"]
+        if self.n_evals % self.update_freq == 0:
+            results_dict = cctbx_score.get_score(
+                hs=self.msmc_m.get_hs(),
+                occs=self.msmc_m.get_w_mat()[:,self.cond],
+                pids=self.pids,
+                f_obs=self.f_obs_filt,
+                r_free_flags=self.flags_filt,
+                target=self.target,
+                update_scale=self.update_scale,
+                update_k1=self.update_k1,
+                u_aniso_file=self.u_aniso_file,
+                delta=self.delta
+            )
 
-        dff_dx_dict = dict()
-        dff_avg_mag = 0
-        dxray_avg_mag = 0
-        for i in range(len(self.pids)):
-            pid = self.pids[i]
-            d = IMP.core.XYZR(self.get_model(), pid)
-            dff_dx_dict[pid] = d.get_derivatives()
-            dff_avg_mag = dff_avg_mag + d.get_derivatives().get_magnitude()
+            score = results_dict["score"]
+            grads_site = results_dict["grads_site"]
+            grads_occ = results_dict["grads_occ"]
+            r_work = results_dict["r_work"]
+            r_free = results_dict["r_free"]
+            r_all = results_dict["r_all"]
 
-            if sa.get_derivative_accumulator():
-                df_dx_vec_3d = IMP.algebra.Vector3D(grads_site[i][0], grads_site[i][1], grads_site[i][2])
+            dff_dx_dict = dict()
+            dff_avg_mag = 0
+            dxray_avg_mag = 0
+            for i in range(len(self.pids)):
+                pid = self.pids[i]
+                d = IMP.core.XYZR(self.get_model(), pid)
+                dff_dx_dict[pid] = d.get_derivatives()
+                dff_avg_mag = dff_avg_mag + d.get_derivatives().get_magnitude()
 
-                # Store the derivative.
-                self.df_dxs[pid] = df_dx_vec_3d
+                if sa.get_derivative_accumulator():
+                    # Store the derivative.
+                    df_dx_vec_3d = IMP.algebra.Vector3D(grads_site[i][0], grads_site[i][1], grads_site[i][2])
+                    self.df_dxs[pid] = df_dx_vec_3d
 
-                dxray_avg_mag = dxray_avg_mag + df_dx_vec_3d.get_magnitude()
+                    dxray_avg_mag = dxray_avg_mag + df_dx_vec_3d.get_magnitude()
 
-        # Calculate and save the weights gradients.
-        n_atoms = len(grads_occ) // self.n_state
-        for i in range(self.n_state):
-            state_grad_occs = grads_occ[i*n_atoms:(i+1)*n_atoms]
-            # state_grad_w = state_grad_occs[0]
-            state_grad_w = sum(state_grad_occs)
-            self.w_grads[i] = state_grad_w
+            self.score = score
+            self.r_free = r_free
+            self.r_work = r_work
+            self.r_all = r_all
+
+        # # Calculate and save the weights gradients.
+        # n_atoms = len(grads_occ) // self.n_state
+        # for i in range(self.n_state):
+        #     state_grad_occs = grads_occ[i*n_atoms:(i+1)*n_atoms]
+        #     # state_grad_w = state_grad_occs[0]
+        #     state_grad_w = sum(state_grad_occs)
+        #     self.w_grads[i] = state_grad_w
 
         if sa.get_derivative_accumulator():
-            dff_avg_mag = dff_avg_mag / len(self.pids)
-            dxray_avg_mag = dxray_avg_mag / len(self.pids)
+            # dff_avg_mag = dff_avg_mag / len(self.pids)
+            # dxray_avg_mag = dxray_avg_mag / len(self.pids)
 
-            df_mag_ratio = dff_avg_mag / dxray_avg_mag
-            self.df_mag_ratio = df_mag_ratio
+            # df_mag_ratio = dff_avg_mag / dxray_avg_mag
+            # self.df_mag_ratio = df_mag_ratio
+            df_mag_ratio = 1
 
             for pid in self.pids:
                 d = IMP.core.XYZR(self.get_model(), pid)
@@ -179,12 +195,9 @@ class XtalRestraint(IMP.Restraint):
 
                 d.add_to_derivatives(dxray_dx_scaled, sa.get_derivative_accumulator())
 
-        sa.add_score(score)
+        sa.add_score(self.score)
+        self.n_evals += 1
         # Store the score.
-        self.score = score
-        self.r_free = r_free
-        self.r_work = r_work
-        self.r_all = r_all
 
 
     def do_get_inputs(self):
