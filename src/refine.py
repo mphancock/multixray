8fcb9e830af73514c7a85d98d4f4a06011df4e1a
mphancock <matthewhancock97@gmail.com>
2024-11-04 09:18:39 -0600

    implemented refinement of full posterior

--- src/refine.py
+++ src/refine.py
@@ -166,3 +166,132 @@ def refine_h(
         cg.add_optimizer_state(o_state)
 
     cg.optimize(n_step)
+
+
+sys.path.append(str(Path(Path.home(), "xray/src")))
+from params import write_params_txt, write_params_csv, read_job_csv
+from miller_ops import get_crystal_symmetry, get_f_obs, get_flags, clean_miller_array
+import multi_state_multi_condition_model
+from charmm import get_charmm_restraint_set, CHARMMDerivHolder
+import xray_restraint
+
+
+def refine_posterior(
+    pdb_file,
+    cif_files,
+    w_mat,
+    ref_pdb_file
+):
+    ### REPRESENTATION
+    # w_mat = np.array([
+    #     [0.9, 0.1],
+    #     [0.1, 0.9]
+    # ])
+
+    # cif_files = [Path(Path.home(), "xray/dev/45_synthetic_native_4/data/cifs/native_0.cif"), Path(Path.home(), "xray/dev/45_synthetic_native_4/data/cifs/native_1.cif")]
+
+    N, J = w_mat.shape
+
+    crystal_symmetries = []
+    for cond in range(J):
+        crystal_symmetries.append(get_crystal_symmetry(
+            cif_file=cif_files[cond]
+        ))
+
+    msmc_m = multi_state_multi_condition_model.MultiStateMultiConditionModel(
+        pdb_files=[pdb_file],
+        w_mat=w_mat,
+        crystal_symmetries=crystal_symmetries
+    )
+
+    ### SCORING
+    m, hs = msmc_m.get_m(), msmc_m.get_hs()
+    # rset_charmm = IMP.RestraintSet(m, 1.0)
+
+    # for h in hs:
+        # charmm_rs = charmm.charmm_restraints(
+        #     m,
+        #     h,
+        #     eps=False
+        # )
+        # rset_charmm.add_restraints(charmm_rs)
+
+    # r_charmm = CHARMMRestraint(msmc_m=msmc_m)
+    r_charmm = get_charmm_restraint_set(m, hs)
+
+    # cif file here is a string.
+    rset_xray = IMP.RestraintSet(m, 1.0)
+    charmm_deriv_holder = CHARMMDerivHolder()
+    for i in range(len(cif_files)):
+        cif_file = cif_files[i]
+        if not cif_file:
+            continue
+
+        # f_obs_array = miller_ops.get_miller_array(
+        #     f_obs_file=cif_file,
+        #     # label="_refln.F_meas_au"
+        #     label="_refln.intensity_meas"
+        # )
+        f_obs = get_f_obs(cif_file=cif_file)
+        f_obs = clean_miller_array(f_obs)
+
+        # Set flags from file.
+        # status_array = miller_ops.get_miller_array(
+        #     f_obs_file=cif_file,
+        #     label="_refln.status"
+        # )
+        # flags_array = status_array.customized_copy(data=status_array.data()=="f")
+        flags = get_flags(cif_file=cif_file)
+        f_obs, flags = f_obs.common_sets(other=flags)
+
+        # print("N_OBS: ", len(f_obs.data()), len(flags.data()))
+        r_xray = xray_restraint.XtalRestraint(
+            msmc_m=msmc_m,
+            cond=i,
+            f_obs=f_obs,
+            free_flags=flags,
+            w_xray=1/len(cif_files),
+            update_scale=True,
+            update_k1=True,
+            update_freq=1,
+            # ref_com=com
+            # shadow_restraint=r_tracker_charmm,
+            # r_charmm=r_charmm,
+            charmm_holder=charmm_deriv_holder,
+            ref_com=None
+        )
+
+        rset_xray.add_restraint(r_xray)
+
+
+    r_xrays = [rset_xray.get_restraint(i) for i in range(rset_xray.get_number_of_restraints())]
+
+    for r_xray in r_xrays:
+        r_xray.evaluate(True)
+
+    sf_refine = IMP.core.RestraintsScoringFunction([r_charmm, rset_xray])
+    cg = IMP.core.ConjugateGradients(msmc_m.get_m())
+    cg.set_scoring_function(sf_refine)
+    cg.optimize(50)
+
+    IMP.atom.write_multimodel_pdb(hs, str(ref_pdb_file))
+
+    score_dict = dict()
+    score_dict["ff"] = r_charmm.evaluate(True)
+
+    # for cif_file in cif_files:
+    for i in range(J):
+        cif_name = cif_files[i].stem
+        score_dict["r_free_{}".format(cif_name)] = r_xrays[i].get_r_free()
+        score_dict["r_work_{}".format(cif_name)] = r_xrays[i].get_r_work()
+        score_dict["xray_{}".format(cif_name)] = r_xrays[i].get_score()
+
+    return score_dict
+
+    # score_dict["r_free_0"] = r_xrays[0].get_r_free()
+    # score_dict["r_free_1"] = r_xrays[1].get_r_free()
+    # score_dict["r_work_0"] = r_xrays[0].get_r_work()
+    # score_dict["r_work_1"] = r_xrays[1].get_r_work()
+    # score_dict["pdb_file"] = pdb_file
+
+    # return score_dict
