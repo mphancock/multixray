ff1571024be1e1aa69e8cb4fb6df97a9c62aa670
mphancock <matthewhancock97@gmail.com>
2024-11-12 08:52:03 -0600

    moved 2 functions to utility

--- src/utility.py
+++ src/utility.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+
 import numpy as np
 
 import IMP
 import IMP.atom
+
+import iotbx.pdb
 
 
 def get_string_from_sample_sched(sa_sched):
@@ -58,3 +61,26 @@ def pool_read_pdb(
         return m, hs
     else:
         return RuntimeError("{} file does not exist".format(pdb_file))
+
+
+def get_crystal_info(pdb_file):
+    pdb_input = iotbx.pdb.hierarchy.input(file_name=str(pdb_file))
+    structure = pdb_input.xray_structure_simple()
+    symmetry = structure.crystal_symmetry()
+    uc = symmetry.unit_cell()
+    uc_dimensions = uc.parameters()
+    sg_info = symmetry.space_group_info()
+    sg_symbol = sg_info.symbol_and_number().split("(")[0]
+
+    return uc_dimensions, sg_symbol
+
+
+def get_ordered_hs(
+        h_0s,
+        occs
+):
+    # Need to invert the list.
+    ids = list(np.argsort(occs))
+    ids.reverse()
+    h_0s_ordered = [h_0s[i] for i in ids]
+    return h_0s_ordered
